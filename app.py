bcb1bf54284a95b16219ec4dbe43ad5541c9095b
Süleyman Sevimli <40808249+suleymansevimli@users.noreply.github.com>
2023-12-15 23:12:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from transformers import pipeline
 model = AutoModelForTokenClassification.from_pretrained('PATH_OF_YOUR_LOCAL_MODEL')
 tokenizer = AutoTokenizer.from_pretrained('PATH_OF_YOUR_LOCAL_MODEL')
 
-nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+# model type ex: ner, text-generation, question-answering ... etc.
+nlp = pipeline("YOUR_MODEL_TYPE", model=model, tokenizer=tokenizer)
 example = "Example Text."
 ner_results = nlp(example)
 print(ner_results)
